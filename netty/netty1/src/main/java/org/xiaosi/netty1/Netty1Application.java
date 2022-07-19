101786e4bd7afc44615c3c1c8892eb53eddb74d7
sgh <8857807@163.com>
2022-07-19 14:03:51 +0800

    first commit

---
+++ netty/netty1/src/main/java/org/xiaosi/netty1/Netty1Application.java
@@ -0,0 +1,18 @@
+package org.xiaosi.netty1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: Netty1Application
+ * @Description:
+ * @Author: 88578
+ * @Date: 2022/7/19 13:54
+ */
+@SpringBootApplication
+public class Netty1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Netty1Application.class,args);
+    }
+}
